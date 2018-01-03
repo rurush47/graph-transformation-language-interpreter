d08e9b9c5c6fdf6ab2145a929f503072a0456fca
Unknown <alex@lichthund.com>
2018-01-03 13:36:13 +0100

    Rename test file

--- Test/LexerTest.cs
+++ Test/LexerTest.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using GraphTransformationLanguage;
 using NUnit.Framework;
 
 namespace Test
 {
     [TestFixture]
-    public class Tests
+    public class LexerTest
     {
         string _testFilesPath = TestContext.CurrentContext.TestDirectory;
         
@@ -24,7 +23,7 @@ namespace Test
                 t = lexer.GetNextToken();
             }
             
-            Assert.True(tokens.Count == 14);
+            Assert.True(tokens.Count == 15);
             Assert.AreEqual(tokens[0].Type, TokenType.Semicolon);
             Assert.AreEqual(tokens[1].Type, TokenType.Colon);
             Assert.AreEqual(tokens[2].Type, TokenType.Equals);
@@ -39,6 +38,7 @@ namespace Test
             Assert.AreEqual(tokens[11].Type, TokenType.Identifier);
             Assert.AreEqual(tokens[12].Type, TokenType.VBar);
             Assert.AreEqual(tokens[13].Type, TokenType.Translate);
+            Assert.AreEqual(tokens[14].Type, TokenType.Start);
         }
 
         [Test]
